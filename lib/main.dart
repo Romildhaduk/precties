ed293cbf80af85edee3bde74e6d738bd8ba4dc68
Romil dhaduk <dhadukromil5@gmail.com>
2022-04-26 08:18:52 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -7,38 +7,7 @@ void main() {
       home: SafeArea(
         child: RichText(
           text: const TextSpan(
-            text: "\n\n\t\t\t\t\t\tName:",
-            style: TextStyle(color: Colors.yellow, fontWeight: FontWeight.bold),
-            children: [
-              TextSpan(
-                text: "\n\t\t\tromil dhaduk",
-                style: TextStyle(color: Colors.red),
-                children: [
-                  TextSpan(
-                    text:
-                    "\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tAge:",
-                    style: TextStyle(
-                        color: Colors.yellow, fontWeight: FontWeight.bold),
-                    children: [
-                      TextSpan(
-                        text:
-                        "\n\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t18",
-                        style: TextStyle(color: Colors.red),
-                        children: [
-                          TextSpan(
 
-                            ],
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
     ),
   );
 }
